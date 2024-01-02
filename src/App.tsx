dfafaf395b0b2d807e3309e3c8bcba8a69e2f0a9
AxelH97 <hilde.axel@web.de>
2024-01-02 13:22:30 +0100

    .

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>  
+    </>
+  )
+}
+
+export default App
